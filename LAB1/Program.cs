fe3ad267c9dc45ff52d56ba6f7696b1219a09bfa
Артем Дармограй <firefuriousfox@gmail.com>
2019-10-14 20:30:32 +0300

    ввод данных

--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -10,7 +10,33 @@ namespace LAB1
     {
         static void Main(string[] args)
         {
+            Console.Title = "Дармограй Артем ИУ5-32Б";
+            bool correct = false;
+            double a = 0, b = 0, c = 0;
+            while (correct == false)
+            {
+                Console.WriteLine("Введите коэффициент a");
+                string str1 = Console.ReadLine();
+                Console.WriteLine("Введите коэффициент b");
+                string str2 = Console.ReadLine();
+                Console.WriteLine("Введите коэффициент c");
+                string str3 = Console.ReadLine();
+                if (double.TryParse(str1, out a) && double.TryParse(str2, out b) && double.TryParse(str3, out c))
+                {
+                    a = double.Parse(str1);
+                    b = double.Parse(str2);
+                    c = double.Parse(str3);
+                    correct = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Коэффициенты введены некорректно.");
+                    Console.WriteLine("Пожалуйста, повторите ввод данных.");
+                    Console.ResetColor();
+                }
 
+            }
 
         }
     }
