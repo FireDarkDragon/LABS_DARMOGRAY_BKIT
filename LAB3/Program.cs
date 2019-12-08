d7d9083521684641e462794dc319c973f2f5d2fa
Артем Дармограй <firefuriousfox@gmail.com>
2019-12-08 19:59:24 +0300

    LAB3 main commit

--- LAB3/Program.cs
+++ LAB3/Program.cs
@@ -1,16 +1,45 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LAB2;
 
 namespace LAB3
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-             
+            Rectangle r = new Rectangle(10, 10);
+            Square s = new Square(10);
+            Circle c = new Circle(1);
+            ArrayList list = new ArrayList();
+            list.Add(r);
+            list.Add(s);
+            list.Add(c);
+            Console.WriteLine("Содержимое площади элементов коллекции ArrayList:");
+            foreach (Figure f in list)
+                Console.WriteLine(f.GetArea());
+            List<Figure> listG = new List<Figure>();
+            foreach (Figure f in list)
+                listG.Add(f);
+            listG.Sort();
+            Console.WriteLine("Содержимое коллекции List<Figure>:");
+            foreach (Figure f in listG)
+                Console.WriteLine(f.ToString());
+            Matrix<Figure> m = new Matrix<Figure>(1, 2, 3, new Square(0));
+            m[0, 0, 0] = r;
+            m[0, 1, 0] = new Rectangle(29, 11);
+            m[0, 0, 1] = s;
+            m[0, 1, 1] = new Square(29);
+            m[0, 0, 2] = c;
+            m[0, 1, 2] = new Circle(29.11);
+            Console.WriteLine(m);
+            SimpleStack<Figure> stack = new SimpleStack<Figure>();
+            stack.Add(m[0, 0, 0]);
+            stack.Pop();
+            stack.Add(s);
+            stack.Add(c);
+            string close = Console.ReadLine();
         }
     }
 }
---
+++ LAB3/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    public class Square : Rectangle, IPrint
+    {
+        public Square(double length) : base(length, length) { }
+        public override string ToString()
+        {
+            return "Квадрат: длина = " + Width + ", площадь = " + GetArea();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
