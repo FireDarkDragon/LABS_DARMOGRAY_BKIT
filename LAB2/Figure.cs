a1147420ef9d2099bf0176566726fa8ff028524a
Артем Дармограй <firefuriousfox@gmail.com>
2019-12-08 18:29:53 +0300

    added figure class

---
+++ LAB2/Figure.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Lab2
+{
+    public abstract class Figure : IComparable<Figure>
+    {
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+        public int CompareTo(Figure f)
+        {
+            return this.GetArea().CompareTo(f.GetArea());
+        }
+    }
+}
